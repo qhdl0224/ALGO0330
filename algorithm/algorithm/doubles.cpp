23bf2129e4ba67563a7fbe9222d4b381d937f6eb
wooyonghwa <qhdl0224@naver.com>
2018-03-30 10:55:39 +0900

    알고리즘

---
+++ algorithm/algorithm/doubles.cpp
@@ -0,0 +1,63 @@
+#pragma warning(disable:4996)
+#include <iostream>
+using namespace std;
+
+int print(int num,int arr[][15]);
+int main()
+{
+	int num;
+	int result;
+	int arr[3][15];
+	freopen("C:\\data\\ex1.txt", "r", stdin);
+	cin >> num;
+
+	for (int i = 0; i < num; i++)
+	{
+		for (int j = 0; j < 15; j++)
+		{
+			cin >> arr[i][j];
+			if (arr[i][j] == 0)
+			{
+				break;
+			}
+		}
+	}
+
+	for (int i = 0; i < num; i++)
+	{
+		result = print(i, arr);
+		cout << "# 수열 : ";
+		for (int j = 0; j < 15; j++)
+		{
+			if (arr[i][j] == 0)
+			{
+				break;
+			}
+			cout << arr[i][j] << ' ';
+		}
+		cout << "\n";
+		cout << "2배 값이 존재하는 원소의 개수는 "<<result<<"개 입니다" << '\n';
+	}
+
+}
+int print(int num,int arr[][15])
+{
+	int cnt = 0;
+	int select = 0;
+
+	while (arr[num][select] != 0)
+	{
+		for (int i = 0; i < 15; i++)
+		{
+			if (arr[num][i] == arr[num][select] * 2)
+			{
+				cnt++;
+			}
+		}
+		select++;
+	}
+
+	return cnt;
+	
+	
+}
